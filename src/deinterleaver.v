a1616472409525ee8ea16774123259ba41fa4467
Shawqeem <1004837646@qq.com>
2024-11-08 19:54:06 +0800

    feat: change the depth of the IT & DIT

--- src/deinterleaver.v
+++ src/deinterleaver.v
@@ -1,6 +1,6 @@
 module deinterleaver # (
     parameter n = 7, // (n,k) Hamming code
-    parameter symbol_num = 5 // data_length = n * symbol_num
+    parameter symbol_num = 4 // data_length = n * symbol_num
 ) (
     input wire clk,
     input wire rst,
@@ -16,40 +16,33 @@ module deinterleaver # (
         end
         else if (r_en) begin
             r_data_o[0] <= r_data_i[0];
-            r_data_o[1] <= r_data_i[5];
-            r_data_o[2] <= r_data_i[10];
-            r_data_o[3] <= r_data_i[15];
-            r_data_o[4] <= r_data_i[20];
-            r_data_o[5] <= r_data_i[25];
-            r_data_o[6] <= r_data_i[30];
+            r_data_o[1] <= r_data_i[4];
+            r_data_o[2] <= r_data_i[8];
+            r_data_o[3] <= r_data_i[12];
+            r_data_o[4] <= r_data_i[16];
+            r_data_o[5] <= r_data_i[20];
+            r_data_o[6] <= r_data_i[24];
             r_data_o[7] <= r_data_i[1];
-            r_data_o[8] <= r_data_i[6];
-            r_data_o[9] <= r_data_i[11];
-            r_data_o[10] <= r_data_i[16];
-            r_data_o[11] <= r_data_i[21];
-            r_data_o[12] <= r_data_i[26];
-            r_data_o[13] <= r_data_i[31];
+            r_data_o[8] <= r_data_i[5];
+            r_data_o[9] <= r_data_i[9];
+            r_data_o[10] <= r_data_i[13];
+            r_data_o[11] <= r_data_i[17];
+            r_data_o[12] <= r_data_i[21];
+            r_data_o[13] <= r_data_i[25];
             r_data_o[14] <= r_data_i[2];
-            r_data_o[15] <= r_data_i[7];
-            r_data_o[16] <= r_data_i[12];
-            r_data_o[17] <= r_data_i[17];
-            r_data_o[18] <= r_data_i[22];
-            r_data_o[19] <= r_data_i[27];
-            r_data_o[20] <= r_data_i[32];
+            r_data_o[15] <= r_data_i[6];
+            r_data_o[16] <= r_data_i[10];
+            r_data_o[17] <= r_data_i[14];
+            r_data_o[18] <= r_data_i[18];
+            r_data_o[19] <= r_data_i[22];
+            r_data_o[20] <= r_data_i[26];
             r_data_o[21] <= r_data_i[3];
-            r_data_o[22] <= r_data_i[8];
-            r_data_o[23] <= r_data_i[13];
-            r_data_o[24] <= r_data_i[18];
-            r_data_o[25] <= r_data_i[23];
-            r_data_o[26] <= r_data_i[28];
-            r_data_o[27] <= r_data_i[33];
-            r_data_o[28] <= r_data_i[4];
-            r_data_o[29] <= r_data_i[9];
-            r_data_o[30] <= r_data_i[14];
-            r_data_o[31] <= r_data_i[19];
-            r_data_o[32] <= r_data_i[24];
-            r_data_o[33] <= r_data_i[29];
-            r_data_o[34] <= r_data_i[34];
+            r_data_o[22] <= r_data_i[7];
+            r_data_o[23] <= r_data_i[11];
+            r_data_o[24] <= r_data_i[15];
+            r_data_o[25] <= r_data_i[19];
+            r_data_o[26] <= r_data_i[23];
+            r_data_o[27] <= r_data_i[27];
             r_eno <= 1;
         end
     end
--- src/interleaver.v
+++ src/interleaver.v
@@ -1,6 +1,6 @@
 module interleaver # (
     parameter n = 7, // (n,k) Hamming code
-    parameter symbol_num = 5 // data_length = n * symbol_num
+    parameter symbol_num = 4 // data_length = n * symbol_num
 ) (
     input wire clk,
     input wire rst,
@@ -19,37 +19,30 @@ module interleaver # (
             data_o[1] <= data_i[7];
             data_o[2] <= data_i[14];
             data_o[3] <= data_i[21];
-            data_o[4] <= data_i[28];
-            data_o[5] <= data_i[1];
-            data_o[6] <= data_i[8];
-            data_o[7] <= data_i[15];
-            data_o[8] <= data_i[22];
-            data_o[9] <= data_i[29];
-            data_o[10] <= data_i[2];
-            data_o[11] <= data_i[9];
-            data_o[12] <= data_i[16];
-            data_o[13] <= data_i[23];
-            data_o[14] <= data_i[30];
-            data_o[15] <= data_i[3];
-            data_o[16] <= data_i[10];
-            data_o[17] <= data_i[17];
-            data_o[18] <= data_i[24];
-            data_o[19] <= data_i[31];
-            data_o[20] <= data_i[4];
-            data_o[21] <= data_i[11];
-            data_o[22] <= data_i[18];
-            data_o[23] <= data_i[25];
-            data_o[24] <= data_i[32];
-            data_o[25] <= data_i[5];
-            data_o[26] <= data_i[12];
-            data_o[27] <= data_i[19];
-            data_o[28] <= data_i[26];
-            data_o[29] <= data_i[33];
-            data_o[30] <= data_i[6];
-            data_o[31] <= data_i[13];
-            data_o[32] <= data_i[20];
-            data_o[33] <= data_i[27];
-            data_o[34] <= data_i[34];
+            data_o[4] <= data_i[1];
+            data_o[5] <= data_i[8];
+            data_o[6] <= data_i[15];
+            data_o[7] <= data_i[22];
+            data_o[8] <= data_i[2];
+            data_o[9] <= data_i[9];
+            data_o[10] <= data_i[16];
+            data_o[11] <= data_i[23];
+            data_o[12] <= data_i[3];
+            data_o[13] <= data_i[10];
+            data_o[14] <= data_i[17];
+            data_o[15] <= data_i[24];
+            data_o[16] <= data_i[4];
+            data_o[17] <= data_i[11];
+            data_o[18] <= data_i[18];
+            data_o[19] <= data_i[25];
+            data_o[20] <= data_i[5];
+            data_o[21] <= data_i[12];
+            data_o[22] <= data_i[19];
+            data_o[23] <= data_i[26];
+            data_o[24] <= data_i[6];
+            data_o[25] <= data_i[13];
+            data_o[26] <= data_i[20];
+            data_o[27] <= data_i[27];
             eno <= 1;
         end
     end
