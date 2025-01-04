6a758a590a38c1886b95518dbe23ae52dd4df8b1
Shawqeem <1004837646@qq.com>
2025-01-04 14:06:36 +0800

    fix: testbench for interleaver

--- test/test_IL.v
+++ test/test_IL.v
@@ -6,12 +6,12 @@ module test_IL();
     reg rst;
 
     reg en;
-    reg [35 -1:0] data = 35'b00010100011111000011110110111100101;
+    reg [28 -1:0] data = 28'b0011111000011110110111100101;
     wire en_IL;
-    wire [35 -1:0] data_IL;
+    wire [28 -1:0] data_IL;
     interleaver il(clk, rst, en, data, en_IL, data_IL);
 
-    wire [35 -1:0] data_DIL;
+    wire [28 -1:0] data_DIL;
     wire en_DIL;
     deinterleaver dil(clk, rst, en_IL, data_IL, en_DIL, data_DIL);
 
