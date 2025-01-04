d028dfed35a8c3afb6ce7f9eb5056bdc5608dfae
Shawqeem <1004837646@qq.com>
2025-01-04 16:25:40 +0800

    feat: finish the simulation for top module

--- test/test_top.v
+++ test/test_top.v
@@ -2,20 +2,30 @@
 
 module test_top();
     
-    reg clk;
+    reg clk_origin;
     reg rst;
+    reg [15:0] button;
+    wire [15:0] hamming_dec;
+    wire hamming_wave;
+    wire interres_wave;
 
     initial begin
-        clk <= 1;
-        rst <= 1;
-        #5
+        clk_origin <= 1;
+        button <= 16'b0001_0100_0111_1100;
         rst <= 0;
+        #5
+        rst <= 1;
     end
-    always #10 clk = ~clk;
+    
+    always #10 clk_origin = ~clk_origin;
 
     top top_0 (
-        .clk(clk),
-        .rst(rst)
+        .clk_origin(clk_origin),
+        .rst(rst),
+        .button(button),
+        .hamming_dec(hamming_dec),
+        .hamming_wave(hamming_wave),
+        .interres_wave(interres_wave)
     );
     
 endmodule
